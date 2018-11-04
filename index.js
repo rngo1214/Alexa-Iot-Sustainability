6e44cc223e3eb72b439ce0e4068b0d365cad51dc
rngo1214 <rngo1214@gmail.com>
2018-11-03 17:34:44 -0700

    Added package.json

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ console.log('Loading Function');
 
 AWS.config.update({region: 'REGION'});
 
+console.log('Loading function');
+
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    event.Records.forEach((record) => {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log('DynamoDB Record: %j', record.dynamodb);
+    });
+    return `Successfully processed ${event.Records.length} records.`;
+};
